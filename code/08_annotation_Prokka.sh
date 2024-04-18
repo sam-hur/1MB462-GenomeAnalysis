d83c0538801614312fd40671bf5009f73a3c64f3
sam-hur <samhurenkamp@gmail.com>
2024-04-18 09:57:17 +0200

    extended mummerplot to spades+prokka annotation

--- code/08_annotation_Prokka.sh
+++ code/08_annotation_Prokka.sh
@@ -5,7 +5,7 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 03:30:00
-#SBATCH -J Assembly_QC_MUMmer
+#SBATCH -J Annotation_Prokka
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user sam.hurenkamp.9631@student.uu.se
 
@@ -15,7 +15,7 @@ t=2
 #modules
 module load \
 	bioinfo-tools \
-	MUMmer/4.0.0rc1 \
+	prokka/1.45-5b58020 \
 
 
 base_dir=/home/samhur/1MB462-GenomeAnalysis/data
@@ -23,10 +23,9 @@ in=$base_dir/raw_data/genomics_data
 out=$base_dir/metadata/QC/genomics_data
 
 
-mkdir -p $out/assemblies_QC/MUMmer
+mkdir -p $out/annotations/Prokka
 assembly=$out/assemblies/flye/pb/assembly.fasta
 assembly2=$out/assemblies/spades/contigs.fasta
-
 ref=/home/samhur/1MB462-GenomeAnalysis/ref_genome
 ref1=$ref/GCF_009734005.1/ref_NCBI.fna
 ref2=$ref/GCA_009734005.2/ref_GenBank.fna # just optional alt.
@@ -34,22 +33,13 @@ ref2=$ref/GCA_009734005.2/ref_GenBank.fna # just optional alt.
 
 set -x
 # ------------------------
-out=$out/assemblies_QC/MUMmer
-
-# nucmer --maxmatch -c 100 -p $out/nucmer $ref1 $assembly 
-nucmer -p $out/nucmer.flye $ref1 $assembly
-delta-filter -1 $out/nucmer.flye.delta > $out/filtered_alignments.flye.delta
-mummerplot --png -p mummerplot.flye $out/filtered_alignments.flye.delta 
-mv mummerplot.* $out
-
-nucmer -p $out/nucmer.spades $ref1 $assembly2
-delta-filter -1 $out/nucmer.spades.delta > $out/filtered_alignments.spades.delta
-mummerplot --png -p mummerplot.spades $out/filtered_alignments.spades.delta 
-mv mummerplot.* $out
-
-
-# show output
-tree $out
+prefix1=prokka_flye
+prefix2=prokka_spades
+out=$out/annotations/Prokka
+prokka --force --outdir $out --prefix $prefix1 \
+    --usegenus --genus Enterococcus --species faecium \
+    --kingdom Bacteria --gram pos --strain E745 \
+    $assembly
 
 # ------------------------
 set +x
