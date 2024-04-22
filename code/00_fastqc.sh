492ffaf45a40f4b34c8a8ffcb95bb6f939867fb9
sam-hur <samhurenkamp@gmail.com>
2024-04-22 16:36:22 +0200

    updated: FASTQC
    Now accommodates RNA reads as well

--- code/00_fastqc.sh
+++ code/00_fastqc.sh
@@ -5,7 +5,7 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 00:30:00
-#SBATCH -J QC_DNA_PRETRIM
+#SBATCH -J QC_PRETRIM
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user sam.hurenkamp.9631@student.uu.se
 
@@ -20,6 +20,27 @@ base_dir=/home/samhur/1MB462-GenomeAnalysis/data
 in=$base_dir/raw_data/genomics_data
 out=$base_dir/metadata/QC/genomics_data
 
+mkdir -p \
+    $out/Illumina \
+    $out/NanoPore \
+    $out/PacBio \
+    $out/RNA-Seq_BHI \
+    $out/RNA-Seq_Serum \
+    $out/TN-Seq/BHI \
+    $out/TN-Seq/Serum \
+    $out/TN-Seq/HSerum
+
 fastqc -o $out/Illumina -t 2 $in/Illumina/*.gz 
 fastqc -o $out/Nanopore $in/Nanopore/*.gz
-fastqc -o $out/PacBio -t 6 $in/PacBio/*.gz
+fastqc -o $out/PacBio -t 2 $in/PacBio/*.gz
+
+# RNA
+fastqc -o $out/RNA-Seq_BHI -t 2 $base_dir/raw_data/transcriptomics_data/RNA-Seq_BH/*.gz
+fastqc -o $out/RNA-Seq_Serum -t 2 $base_dir/raw_data/transcriptomics_data/RNA-Seq_Serum/*.gz
+
+fastqc -o $out/TN-Seq/BHI -t 2 $base_dir/raw_data/transcriptomics_data/Tn-Seq_BHI/*.gz
+fastqc -o $out/TN-Seq/Serum -t 2 $base_dir/raw_data/transcriptomics_data/Tn-Seq_Serum/*.gz
+fastqc -o $out/TN-Seq/HSerum -t 2 $base_dir/raw_data/transcriptomics_data/Tn-Seq_HSerum/*.gz
+
+
+
